87dda7644494e856510f27b72ac3c79341aa9d7c
Minsuk <msk0519@gmail.com>
2026-08-21 11:17:04 -0700

    add event marker
    CPU and GPU will use exactly same interface for event marker
    
    TODO : need to combine this into one

---
+++ Engine/include/system/eventMarker.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+#include <system/defines.hpp>
+#include <system/eventsDefines.hpp>
+
+#if ENGINE_DEBUG_GPUEVENT
+#include <d3d12.h>
+#endif // ENGINE_DEBUG_GPUEVENT
+
+namespace prof
+{
+#if ENGINE_DEBUG_GPUEVENT
+	class ScopedGPUEvent
+	{
+	public:
+		ScopedGPUEvent(ID3D12GraphicsCommandList* cmdList, EVENT_INDEX nameID);
+		~ScopedGPUEvent();
+
+		ScopedGPUEvent(const ScopedGPUEvent&) = delete;
+		ScopedGPUEvent& operator=(const ScopedGPUEvent&) = delete;
+
+	private:
+		ID3D12GraphicsCommandList* cmdList;
+		int profEvent = -1;
+	};
+
+	#define GPU_EVENT_CONCAT_(a, b) a##b
+	#define GPU_EVENT_CONCAT(a, b) GPU_EVENT_CONCAT_(a, b)
+	#define GPU_EVENT(cmdList, name) \
+		prof::ScopedGPUEvent GPU_EVENT_CONCAT(gpuScopedEvent_, __COUNTER__)((cmdList), prof::eventID<name>)
+#else
+	#define GPU_EVENT(cmdList, name) ((void)(cmdList))
+#endif // ENGINE_DEBUG_GPUEVENT
+
+#if ENGINE_DEBUG_CPUEVENT
+	class ScopedCPUEvent
+	{
+	public:
+		ScopedCPUEvent(EVENT_INDEX nameID);
+		~ScopedCPUEvent();
+
+		ScopedCPUEvent(const ScopedCPUEvent&) = delete;
+		ScopedCPUEvent& operator=(const ScopedCPUEvent&) = delete;
+
+	private:
+		int profEvent = -1;
+	};
+
+	#define CPU_EVENT_CONCAT_(a, b) a##b
+	#define CPU_EVENT_CONCAT(a, b) CPU_EVENT_CONCAT_(a, b)
+	#define CPU_EVENT(name) \
+		prof::ScopedCPUEvent CPU_EVENT_CONCAT(cpuScopedEvent_, __COUNTER__)(prof::eventID<name>)
+#else
+	#define CPU_EVENT(name) ((void)0)
+#endif // ENGINE_DEBUG_CPUEVENT
+
+};  // namespace prof
---
+++ Engine/src/system/eventMarker.cpp
@@ -0,0 +1,56 @@
+#include <system/eventMarker.hpp>
+#include <system/eventProf.hpp>
+
+#include <cstring>
+
+#if ENGINE_DEBUG_GPUEVENT
+
+namespace prof
+{
+	ScopedGPUEvent::ScopedGPUEvent(ID3D12GraphicsCommandList* cmdList, EVENT_INDEX nameID)
+		: cmdList(cmdList)
+	{
+		const char* name = prof::getEventName(nameID);
+		cmdList->BeginEvent(1, name, (UINT)strlen(name) + 1);
+#if ENGINE_DEBUG_GPUPROF
+		profEvent = prof::beginGPUProfEvent(cmdList, nameID);
+#endif // ENGINE_DEBUG_GPUPROF
+	}
+
+	ScopedGPUEvent::~ScopedGPUEvent()
+	{
+#if ENGINE_DEBUG_GPUPROF
+		if (profEvent >= 0)
+		{
+			prof::endGPUProfEvent(cmdList, profEvent);
+		}
+#endif // ENGINE_DEBUG_GPUPROF
+		cmdList->EndEvent();
+	}
+}
+
+#endif // ENGINE_DEBUG_GPUEVENT
+
+#if ENGINE_DEBUG_CPUEVENT
+
+namespace prof
+{
+	ScopedCPUEvent::ScopedCPUEvent(EVENT_INDEX nameID)
+	{
+#if ENGINE_DEBUG_CPUPROF
+		profEvent = prof::beginCPUProfEvent(nameID);
+#endif // ENGINE_DEBUG_CPUPROF
+	}
+
+	ScopedCPUEvent::~ScopedCPUEvent()
+	{
+#if ENGINE_DEBUG_CPUPROF
+		if (profEvent >= 0)
+		{
+			prof::endCPUProfEvent(profEvent);
+		}
+#endif // ENGINE_DEBUG_CPUPROF
+	}
+}
+
+#endif // ENGINE_DEBUG_CPUEVENT
