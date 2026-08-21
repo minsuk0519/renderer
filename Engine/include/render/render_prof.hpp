ebe10ad4bce66cc9c50856a8de359b8647e1a462
Minsuk <msk0519@gmail.com>
2026-08-21 11:16:01 -0700

    fixed the previous commit error

--- Engine/include/render/render_prof.hpp
+++ Engine/include/render/render_prof.hpp
@@ -1,1 +1,18 @@
-	// GPU profiler interface: these functions handle all D3D12-specific query heap operations
+#pragma once
+
+#include <system/defines.hpp>
+#include <system/eventsDefines.hpp>
+
+#if ENGINE_DEBUG_GPUPROF
+
+namespace render
+{
+	bool initGPUProfBackend();
+	void closeGPUProfBackend();
+	void endGPUProfFrameBackend();
+	int  beginGPUProfEventBackend(void* cmdList, prof::EVENT_INDEX nameID);
+	void endGPUProfEventBackend(void* cmdList, int eventIndex);
+
+}  // namespace render
+
+#endif // ENGINE_DEBUG_GPUPROF
